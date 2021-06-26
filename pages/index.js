00876fe7eab3e8fd9d1047aa6a2de1fa59010acd
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-06-25 23:20:50 -0500

    Finalizacion home page portfolio.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { Layout } from './components/Layout';
+import { Layout } from "./components/Layout";
+import { experiences, skills, projects } from "./../profile";
+import Link from "next/link";
 
 const Index = () => (
   <Layout>
@@ -8,7 +10,7 @@ const Index = () => (
         <div className="card card-body bg-secondary text-light">
           <div className="row">
             <div className="col-md-4">
-              <img src="./images/ryan-ray-profile2.jpeg" alt=""  className='img-fluid'/>
+              <img src="./images/ryan-ray-profile2.jpeg" alt="" className="img-fluid" />
             </div>
             <div className="col-md-8">
               <h1>Rayan ray</h1>
@@ -20,7 +22,80 @@ const Index = () => (
         </div>
       </div>
     </header>
+
+    {/* Second Section */}
+    <div className="row py-2">
+      <div className="col-md-4">
+        <div className="card bg-light">
+          <div className="card-body">
+            <h1>Skills</h1>
+            {skills.map(({ skill, percentage }, i) => (
+              <div className="py-3" key={i}>
+                <h5>{skill}</h5>
+                <div className="progress">
+                  <div className="progress-bar" role="progressbar" style={{ width: `${percentage}%` }}></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="col-md-8">
+        <div className="card bg-light">
+          <div className="card-body">
+            <h1>Experience</h1>
+
+            <ul>
+              {experiences.map(({ title, description, from, to }, i) => (
+                <li key={i}>
+                  <h3>{title}</h3>
+                  <h5>
+                    {from} - {to}
+                  </h5>
+                  <p>{description}</p>
+                </li>
+              ))}
+            </ul>
+
+            <div className="row text-center">
+              <div className="col-md-12">
+                <Link href="/experiences">
+                  <a className="btn btn-primary text-center">Experiences</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* Portfolio */}
+
+    <div className="row">
+      <div className="card card-body bg-dark">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center text-light">Portfolio</h1>
+          </div>
+
+          {projects.map(({ name, description, image }, index) => (
+            <div className="col-md-4 py-3">
+              <div className="card h-100">
+                <img src={`./images/${image}`} alt="portfolio" />
+                <div className="card-body">
+                  <h3>{name}</h3>
+                  <p>{description}</p>
+                  <Link href="#!">
+                    <a>Conoce mas..</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   </Layout>
-)
+);
 
 export default Index;
---
+++ profile.js
@@ -0,0 +1,63 @@
+export const skills = [
+  { skill: 'Javascript', percentage: 100 },
+  { skill: 'React', percentage: 60 },
+  { skill: 'Angular', percentage: 80 },
+  { skill: 'Java', percentage: 90 },
+  { skill: 'Jest', percentage: 60 },
+  { skill: 'Nextjs', percentage: 70 }
+];
+
+
+export const experiences = [
+  {
+    title: 'Software Developer',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam laudantium omnis nobis deserunt dolore harum reiciendis explicabo accusamus vitae eligendi rem maxime.',
+    from: 2009,
+    to: 2011
+  },
+  {
+    title: 'Fullstack Developer',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam laudantium omnis nobis deserunt dolore harum reiciendis explicabo accusamus vitae eligendi rem maxime.',
+    from: 2011,
+    to: 2016
+  },
+  {
+    title: 'Frontend Developer',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam laudantium omnis nobis deserunt dolore harum reiciendis explicabo accusamus vitae eligendi rem maxime, unde minus dignissimos delectus consequuntur similique adipisci. Voluptates.',
+    from: 2016,
+    to: 2019
+  }
+];
+
+export const projects = [
+  {
+    name: 'Awesome site 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio1.jpeg'
+  },
+  {
+    name: 'Awesome site 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio2.jpg'
+  },
+  {
+    name: 'Awesome site 3',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio3.png'
+  },
+  {
+    name: 'Awesome site 4',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio4.png'
+  },
+  {
+    name: 'Awesome site 5',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio5.jpeg'
+  },
+  {
+    name: 'Awesome site 6',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum qui enim totam culpa',
+    image: 'portfolio6.jpeg'
+  }
+];
